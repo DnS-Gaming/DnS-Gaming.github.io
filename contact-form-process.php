7d8f2594c1f4881c6b7bc1928bb684776a24bfbf
DnS-Gaming <117047176+DnS-Gaming@users.noreply.github.com>
2022-10-31 03:58:25 -0400

    Create contact-form-process.php

---
+++ contact-form-process.php
@@ -0,0 +1,37 @@
+<?php
+if (isset($_POST['demo-email'])) {
+
+    // EDIT THE FOLLOWING TWO LINES:
+    $email_to = "dnsgamingdeveloper@gmail.com";
+    $email_subject = "Tester";
+
+    $name = $_POST['demo-name']; // required
+    $email = $_POST['demo-email']; // required
+    $message = $_POST['demo-message']; // required
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
+
+    Thanks for getting in touch. We'll get back to you soon.
+
+<?php
+}
+?>
